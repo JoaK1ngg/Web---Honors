a2d31a2398f40db1e8e17f630c25c0d6fa82815b
JoaK1ngg <168322916+JoaK1ngg@users.noreply.github.com>
2025-05-18 03:47:44 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,34 @@ document.getElementById("loginForm").addEventListener("submit", (e) => {
   }, 1000);
 });
 
-document.getElementById("apiButton").onclick = async () => {
+async function fetchData() {
   const res = await fetch("https://api.chucknorris.io/jokes/random");
   const data = await res.json();
   document.getElementById("apiResult").textContent = data.value;
-};
+}
+
+
+    document.getElementById("toggle-dark").addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+    });
+
+    document.getElementById("contactForm").addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const nombre = document.getElementById("nombre").value;
+      const email = document.getElementById("email").value;
+      const mensaje = document.getElementById("mensaje").value;
+
+      const response = await fetch("http://localhost:3000/contacto", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nombre, email, mensaje })
+      });
+
+      if (response.ok) {
+        document.getElementById("contactoMensaje").textContent = "Mensaje enviado con éxito.";
+      } else {
+        document.getElementById("contactoMensaje").textContent = "Error al enviar el mensaje.";
+      }
+    });
